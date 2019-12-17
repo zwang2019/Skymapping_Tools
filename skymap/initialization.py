887ddabf6fb700261061c86ca245034002f5e202
zhao <zhao.wang.unsw@gmail.com>
2019-12-17 19:54:48 +1100

    first try

---
+++ skymap/initialization.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+' initialize the raw sequence data '
+
+__author__ = 'Tansel, Zhao'
+
+
+#from XX import XX
+
+
+class Initialization(object):
+    def __init__(self, r=1):
+        self.r = r
+
+    def greeting(self):
+
+        if self.r == 1:
+            print("This is the first stone.")
+
+        return
+
