a63463001c2071cb5a4a5c24cc67acd2899e0302
zhao <zhao.wang.unsw@gmail.com>
2020-01-29 19:12:34 +1100

    update

--- skymapper/core.py
+++ skymapper/core.py
@@ -27,6 +27,8 @@ from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, LearningRat
 from tensorflow.python.client import device_lib
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras.utils import multi_gpu_model
+# from tensorflow.keras import backend as K
 import skymapper.skymapper_trainer as st
 
 
@@ -261,18 +263,18 @@ class Initialization(object):
         print('Val X=', self.X_validation.shape)
         print('Val y=', self.Y_validation.shape)
 
-    # return X, X_test, X_validation, y, Y_test, Y_validation
+        # return X, X_test, X_validation, y, Y_test, Y_validation
 
     def train(self, X_train, X_test, X_validation, Y_train, Y_test, Y_validation, version=2, depth=20, epochs=60):
 
         strategy = tf.distribute.MirroredStrategy()
         if self.choose_gpu is not []:
             # devices:
-            st.get_gpu(self.choose_gpu)
-            print(st._get_available_devices())
-            print('Number of devices: {}'.format(strategy.num_replicas_in_sync))   # may have problem
+            get_gpu(choose_gpu)
+            print(_get_available_devices())
+            print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-        gpus = len(self.choose_gpu)
+        gpus = len(choose_gpu)
 
         tensorboard = TensorBoard(log_dir="log-full/{}".format(time.time()))
 
@@ -466,10 +468,6 @@ class Initialization(object):
 
 
 
-
-
-
-
 class Formatter(object):
     def __init__(self, im, dim, pix_labels):
         self.im = im
@@ -486,3 +484,302 @@ class Formatter(object):
         if index < len(self.pix_labels):
             label = self.pix_labels.iloc[index].name
         return label
+
+### training fuction ###
+
+def residual_block_unit(inputs,
+                        num_filters=16,
+                        kernel_size=(3, 3),
+                        strides=(1, 1),
+                        activation='relu',
+                        batch_normalization=True,
+                        conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        net (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  data_format='channels_first',
+                  kernel_regularizer=l2(1e-4))
+
+    net = inputs
+    if conv_first:
+        net = conv(net)
+        print("Convolution name= ", net.name, " numfilters=", num_filters, " kernel_size=", kernel_size, " strides=",
+              strides)
+        if batch_normalization:
+            net = BatchNormalization()(net)
+            print("Batch normalisation")
+        if activation is not None:
+            net = Activation(activation)(net)
+            print("Activation")
+    else:
+        if batch_normalization:
+            net = BatchNormalization()(net)
+            print("Batch normalisation")
+        if activation is not None:
+            net = Activation(activation)(net)
+            print("Activation")
+        net = conv(net)
+        print("Convolution name= ", net.name, " numfilters=", num_filters, " kernel_size=", kernel_size, " strides=",
+              strides)
+    # conv.name = conv.name + '_' + str(kernel_size) + 'x' + str(kernel_size) + '_' + str(num_filters) + '_' + str(
+    #    strides)  # can be exam later
+    return net
+
+
+def resnet_v1(input_shape, depth, num_classes=4):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    x = 1
+
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    # Start model definition.
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    net = residual_block_unit(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            short_cut = residual_block_unit(inputs=net,
+                                            num_filters=num_filters,
+                                            strides=strides)
+            short_cut = residual_block_unit(inputs=short_cut,
+                                            num_filters=num_filters,
+                                            activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # changed dims
+                net = residual_block_unit(inputs=net,
+                                          num_filters=num_filters,
+                                          kernel_size=1,
+                                          strides=strides,
+                                          activation=None,
+                                          batch_normalization=False)
+            net = tf.keras.layers.add([net, short_cut])
+            net = Activation('relu')(net)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    net = AveragePooling2D(pool_size=8)(net)
+    short_cut = Flatten()(net)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(short_cut)
+
+    # the original version is y, but may cause some problem. Have to exam in the test.
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def resnet_v2(input_shape, depth, num_classes=4):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:777
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = residual_block_unit(inputs=inputs,
+                            num_filters=num_filters_in,
+                            conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2  # downsample
+
+            # bottleneck residual unit
+            y = residual_block_unit(inputs=x,
+                                    num_filters=num_filters_in,
+                                    kernel_size=1,
+                                    strides=strides,
+                                    activation=activation,
+                                    batch_normalization=batch_normalization,
+                                    conv_first=False)
+            y = residual_block_unit(inputs=y,
+                                    num_filters=num_filters_in,
+                                    conv_first=False)
+            y = residual_block_unit(inputs=y,
+                                    num_filters=num_filters_out,
+                                    kernel_size=1,
+                                    conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = residual_block_unit(inputs=x,
+                                        num_filters=num_filters_out,
+                                        kernel_size=1,
+                                        strides=strides,
+                                        activation=None,
+                                        batch_normalization=False)
+            x = tf.keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # why here use y instead of x ?
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 10, 20, 30, 50 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 60:
+        lr *= 1e-4
+    elif epoch > 55:
+        lr *= 1e-3
+    elif epoch > 40:
+        lr *= 1e-2
+    elif epoch > 20:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+
+def config():
+    print('''                                 Model parameter
+         ----------------------------------------------------------------------------
+                   |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+         Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+                   |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+         ----------------------------------------------------------------------------
+         ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+         ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+         ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+         ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+         ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+         ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+         ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+         ---------------------------------------------------------------------------''')
+
+
+def get_gpu(ggg):
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    for gpu in ggg:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+
+def _get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+
+def let_tansel_know(str):
+    print(str)
+
+
+
+
+
+
+
+
+
